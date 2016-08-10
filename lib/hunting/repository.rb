0d653c4ec60c0a12826a5aa371d6d4d587e62fae
Andrew Weidner <metaweidner@gmail.com>
2016-08-10 08:49:07 -0500

    add error handling for missing config or aliases

--- lib/hunting/repository.rb
+++ lib/hunting/repository.rb
@@ -10,14 +10,27 @@ class Repository
   def initialize(aliases)
     @progressbar = Hunting.progressbar('repository', Hunting.config[:cdm]['name'])
     @size = 0
+    @not_found = []
     @collections = scout(aliases)
     @progressbar.finish
+    if @not_found.size > 0
+      print 'Scout failed for: '
+      @not_found.each {|collection| print "#{collection} "}
+      print "\n"
+    end
   end
 
   def scout(aliases)
     collections = {}
     collection_info = {}
-    cdm_collection_data = JSON.parse(open(Hunting.config[:dmwebservices] + "dmGetCollectionList/json").read)
+
+    if Hunting.config[:dmwebservices] == nil
+      @progressbar.finish
+      abort("\nPlease configure Hunting for your CONTENTdm server:
+             \nHunting.configure(config_hash)\nOR\nHunting.configure_with(\"/path/to/config.yml\")\n")
+    else
+      cdm_collection_data = JSON.parse(open(Hunting.config[:dmwebservices] + "dmGetCollectionList/json").read)
+    end
 
     cdm_collection_data.each do |collection|
       collection_info.store(collection['secondary_alias'], {:alias => collection['secondary_alias'], :name => collection['name']})
@@ -29,7 +42,11 @@ class Repository
       end
     else
       aliases.each do |c_alias|
-        collections.store(c_alias, Collection.new({:alias => c_alias, :name => collection_info[c_alias][:name]}))
+        if collection_info[c_alias] == nil
+          @not_found.push(c_alias)
+        else
+          collections.store(c_alias, Collection.new({:alias => c_alias, :name => collection_info[c_alias][:name]}))
+        end
       end
     end
     collections.each {|c_alias, collection| @size += collection.size}
